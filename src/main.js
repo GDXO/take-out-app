fc62496f77f5d378eea8aaadf9096c77a039032b
GDXO <liuchaoyanzi1103@163.com>
2023-12-25 14:54:19 +0800

    fix: 修改了 npm install 的情况下无 mock 数据

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/css/index.less'
-import '@/assets/js/iconfont'
 
 Vue.config.productionTip = false
 
